7f764171d8772fc45f78138bdfd2088f3e219b73
Nicolas Chicunque <nico.chicunque@unity3d.com>
2021-10-13 11:48:18 -0500

    Primer commit, MouthManager.cs

---
+++ Assets/Scripts/MouthManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class MouthManager : MonoBehaviour
+{
+    [SerializeField] private ARFace arFace;
+    [SerializeField] private Text infoText;
+
+    void Start()
+    {
+        arFace.updated += (arFaceUpdatedEventArgs) => {
+            infoText.text = arFaceUpdatedEventArgs.face.vertices[12].ToString() + " ... " +arFaceUpdatedEventArgs.face.vertices[14].ToString();
+        };
+    }
+}
